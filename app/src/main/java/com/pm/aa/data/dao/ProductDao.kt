ae845d438c376c4a8d63e19fdbc2c2045cfdaac0
Sepakuma <jmanuelsantos@ipvc.pt>
2021-11-02 17:01:17 +0000

    Gradles config.  - Plugins  - Depencies  - Packages  .. Criação do Dao e do Product na entities

---
+++ app/src/main/java/com/pm/aa/data/dao/ProductDao.kt
@@ -0,0 +1,20 @@
+package com.pm.aa.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.pm.aa.data.entities.Product
+
+interface ProductDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addProduct(product: Product)
+
+    @Update
+    fun updateProduct(product: Product)
+
+    @Query("SELECT * FROM product ORDER BY id DESC")
+    fun readAllProducts(): LiveData<List<Product>>
+
+    @Delete
+    fun deleteProduct(product: Product)
+}
